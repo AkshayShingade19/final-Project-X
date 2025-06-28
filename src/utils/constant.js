927afc284b209bc2e3bb2c977f5ae95864d34db4
Akshay shingade <akshaykumar200219@gmail.com>
2025-06-28 15:00:15 +0530

    send ropo

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+// export const USER_API_END_POINT = "http://final-twitter-backend-3g44.onrender.com/api/v1/user"
+// export const TWEET_API_END_POINT ="http://final-twitter-backend-3g44.onrender.com/api/v1/user"
+
+export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
